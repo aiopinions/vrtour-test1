72bb6c8d7187675a41c817fa1819903be3839c49
aiopinions <161578121+aiopinions@users.noreply.github.com>
2025-12-24 22:28:53 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kpno-drone-360-2-cc2",
+      "name": "KPNO-Drone-360-2-CC2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.012013330887121754,
+        "pitch": -0.14454176751549497,
+        "fov": 1.4724352309223254
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
